13ec663602182fd011fd943e3bc059c695883364
Xvyce <77040737+Xvyce@users.noreply.github.com>
2021-06-30 15:37:03 +0800

    testing feed system

--- Assets/JuwanShts/gameManager.cs
+++ Assets/JuwanShts/gameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
+using System;
 
 public class gameManager : MonoBehaviour
 {
@@ -11,9 +12,34 @@ public class gameManager : MonoBehaviour
 
     public GameObject pet;
 
+    public GameObject foodPanel;
+    public Sprite[] foodIcon;
+
     private void Update()
     {
         happinessText.GetComponent<Text>().text = pet.GetComponent<Pet>().checkHapiness.ToString();
         hungerText.GetComponent<Text>().text = pet.GetComponent<Pet>().checkHunger.ToString();
+    }
+
+    public void buttonBehavior (int i)
+    {
+        switch (i)
+        {
+            case (0):
+            default:
+                foodPanel.SetActive(!foodPanel.activeInHierarchy);
+                break;
+
+        }
+    }
+
+    public void selectFood(int i)
+    {
+        Toggle(foodPanel);
+    }
+
+    private void Toggle(GameObject foodPanel)
+    {
+        throw new NotImplementedException();
     }
 }
