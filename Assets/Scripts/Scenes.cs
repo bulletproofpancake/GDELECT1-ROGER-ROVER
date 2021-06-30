cebe15650044e29e027d437817839b0dc9fbd9bc
Jolo Gutierrez <57520402+bulletproofpancake@users.noreply.github.com>
2021-06-30 12:41:01 +0800

    added buttons to change scenes

---
+++ Assets/Scripts/Scenes.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Core;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenes : Singleton<Scenes>
+{
+    public void StartGame(string sceneName)
+    {
+        //AudioManager.Instance.Play("button");
+        SceneManager.LoadScene(sceneName);
+        //GameManager.Instance.StartGame();
+        
+    }
+
+    public void Load(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
