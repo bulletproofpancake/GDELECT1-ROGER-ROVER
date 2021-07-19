3815887eac45cc18bd2461762145f6758565a74a
Jolo Gutierrez <57520402+bulletproofpancake@users.noreply.github.com>
2021-07-19 15:21:47 +0800

    can now store entry and exit time

--- Assets/JuwanShts/gameManager.cs
+++ Assets/JuwanShts/gameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
@@ -7,15 +8,37 @@ using System;
 public class gameManager : MonoBehaviour
 {
 
-    public GameObject happinessText;
-    public GameObject hungerText;
+    public Text happinessText;
+    public Text hungerText;
 
-    public GameObject pet;
+    public Pet pet;
+
+
+    private void Awake()
+    {
+        SetEntryTime();
+    }
+
+    private static void SetEntryTime()
+    {
+        PlayerPrefs.SetString("entryTime", DateTime.Now.ToString());
+        Debug.LogWarning(PlayerPrefs.GetString("entryTime"));
+        Debug.LogWarning(PlayerPrefs.GetString("exitTime"));
+    }
+
+    public void SetExitTime()
+    {
+        PlayerPrefs.SetString("exitTime", DateTime.Now.ToString());
+        Debug.LogWarning(PlayerPrefs.GetString("exitTime"));
+    }
 
     private void Update()
     {
-        happinessText.GetComponent<Text>().text = pet.GetComponent<Pet>().checkHapiness.ToString();
-        hungerText.GetComponent<Text>().text = pet.GetComponent<Pet>().checkHunger.ToString();
+        happinessText.text = pet.checkHapiness.ToString();
+        hungerText.text = pet.checkHunger.ToString();
+        
+        if(Input.GetKeyDown(KeyCode.Space)) SetExitTime();
+
     }
 
 
