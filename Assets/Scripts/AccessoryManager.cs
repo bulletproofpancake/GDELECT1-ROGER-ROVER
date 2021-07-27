24ad436b0ab7547508dd1810e1d13d021ce57e1b
Jolo Gutierrez <57520402+bulletproofpancake@users.noreply.github.com>
2021-07-27 22:24:27 +0800

    accessories now get saved

---
+++ Assets/Scripts/AccessoryManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccessoryManager : MonoBehaviour
+{
+    [SerializeField] private GameObject hat, satellite, drone;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        ShowItem("hat");
+        ShowItem("satellite");
+        ShowItem("drone");
+    }
+
+    private void ShowItem(string itemName)
+    {
+        var status = PlayerPrefs.GetInt(itemName + "_bought");
+        switch (itemName)
+        {
+            case "hat":
+                switch (status)
+                {
+                    case 0:
+                        hat.SetActive(false);
+                        break;
+                    case 1:
+                        hat.SetActive(true);
+                        break;
+                }
+
+                break;
+            case "satellite":
+                switch (status)
+                {
+                    case 0:
+                        satellite.SetActive(false);
+                        break;
+                    case 1:
+                        satellite.SetActive(true);
+                        break;
+                }
+
+                break;
+            case "drone":
+                switch (status)
+                {
+                    case 0:
+                        drone.SetActive(false);
+                        break;
+                    case 1:
+                        drone.SetActive(true);
+                        break;
+                }
+
+                break;
+        }
+    }
+    
+    public void BuyItem(string itemName)
+    {
+        PlayerPrefs.SetInt(itemName+"_bought",1);
+    }
+    
+}
