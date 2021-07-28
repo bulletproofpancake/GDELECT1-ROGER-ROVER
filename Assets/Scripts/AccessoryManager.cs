ac091524d99208a3e63cd4c52d06a80960269020
Jolo Gutierrez <57520402+bulletproofpancake@users.noreply.github.com>
2021-07-28 09:10:50 +0800

    buy button no longer unequips

--- Assets/Scripts/AccessoryManager.cs
+++ Assets/Scripts/AccessoryManager.cs
@@ -85,16 +85,7 @@ public class AccessoryManager : MonoBehaviour
     public void BuyItem(string itemName)
     {
         var status = PlayerPrefs.GetInt(itemName + "_bought");
-
-        switch (status)
-        {
-            case 0:
-                PlayerPrefs.SetInt(itemName+"_bought",1);
-                break;
-            case 1:
-                PlayerPrefs.SetInt(itemName+"_bought",0);
-                break;
-        }
+        PlayerPrefs.SetInt(itemName+"_bought",1);
     }
     
 }
